23ef0fcd70d47d67b88530030fb6207d0e583461
Thomas C <thomas1.chezaud@epitech.eu>
2021-03-09 16:44:35 +0100

    Form Login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,42 +1,37 @@
 /*index.jsx*/
+import { borderRadius } from "@material-ui/system";
 import React from "react";
-//Functional Component 
+import Button from "@material-ui/core/Button"
+import { Checkbox } from '@material-ui/core';
+import { FormControlLabel } from '@material-ui/core';
+
 const LoginPage = () => {
     return (
         <div style={{
-            width: "50%",
+            width: "40%",
             margin: "0 auto",
-            padding: "50px"
+            padding: "40px"
         }}>
             <form>
-            <div class="form-outline mb-4">
-                <label class="form-label" for="form1Example1">Email address  </label>
-                <input type="email" id="form1Example1" class="form-control" />
-            </div>
-            <div class="form-outline mb-4">
-                <label class="form-label" for="form1Example2">Password  </label>
-                <input type="password" id="form1Example2" class="form-control" />
-            </div>
+                <div class="form-outline mb-4">
+                    <label class="form-label" for="form1Example1">Email address  </label>
+                    <input type="email" id="form1Example1" class="form-control" />
+                </div>
+                <div class="form-outline mb-4">
+                    <label class="form-label" for="form1Example2">Password  </label>
+                    <input type="password" id="form1Example2" class="form-control" />
+                </div>
 
-            <div class="row mb-4">
-                <div class="col d-flex justify-content-center">
-                    <div class="form-check">
-                        <input
-                            class="form-check-input"
-                            type="checkbox"
-                            value=""
-                            id="form1Example3"
-                            checked
-                        />
-                        <label class="form-check-label" for="form1Example3"> Remember me </label>
+                <div class="row mb-4">
+                    <div class="col d-flex justify-content-center">
+                        <FormControlLabel control={<Checkbox name="checkedC" />} label="Remember me" />
                     </div>
                 </div>
-
-                <div class="col">
-                    <a href="#!">Forgot password?</a>
-                </div>
-            </div>
-            <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                <Button variant="outlined" style={{
+                    width: "100%",
+                }}>
+                    Login
+                </Button>
             </form>
         </div >
     );
