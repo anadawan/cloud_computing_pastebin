10b124eb931def97faab99119c56ad6f76bfef3c
Thomas C <thomas1.chezaud@epitech.eu>
2021-03-11 18:35:25 +0100

    Added support page

--- src/components/support/support.js
+++ src/components/support/support.js
@@ -1,14 +1,33 @@
-/*support.js*/
+/*index.jsx*/
+import React from "react";
+import Button from "@material-ui/core/Button"
+import { Checkbox } from '@material-ui/core';
+import { FormControlLabel } from '@material-ui/core';
 
-const SupportPage = () => {
+const LoginPage = () => {
     return (
-        
         <div style={{
-            width: "100%",
+            width: "40%",
             margin: "0 auto",
-            padding: "50px" 
-        }}>Not yet</div>
+            padding: "40px"
+        }}>
+            <form>
+                <div class="form-outline mb-4">
+                    <label class="form-label" for="form1Example1">Your email address  </label>
+                    <input type="email" id="form1Example1" class="form-control" />
+                </div>
+                <div class="form-outline mb-4">
+                    <label class="form-label" for="form1Example2">Describe your problem  </label>
+                    <textarea id="form1Example2" class="form-control" />
+                </div>
+                <Button variant="outlined" style={{
+                    width: "100%",
+                }}>
+                    Send
+                </Button>
+            </form>
+        </div >
     );
 };
 
-export default SupportPage;
+export default LoginPage;
