11c01473db150d3748ee01011f2b1738c0b08829
Capuch <thomas1.chezaud@epitech.eu>
2021-04-06 14:42:35 +0200

    Upload - minimal

--- src/components/account/profile/profile.js
+++ src/components/account/profile/profile.js
@@ -5,6 +5,14 @@ import {
 import LogoutButton from "./logout_button"
 
 class ProfilePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            file: null,
+            submitted: false,
+            url_file: ""
+        }
+    }
     async componentDidMount() {
         let key = this.props.user.storage[Object.keys(this.props.user.storage)[0]];
         console.log(key);
@@ -17,11 +25,31 @@ class ProfilePage extends Component {
         let res = await fetch("https://b7yutqw6pf.execute-api.us-east-1.amazonaws.com/api/checktoken", reqOpt)
         console.log(await res.json())
     }
+    changeHandler = (e) => {
+        let filen = e.target.files[0];
+
+        this.setState({ file: filen });
+        this.setState({ submitted: true })
+        console.log(e.target.files[0])
+    }
+    handleSubmission = async () => {
+        const formData = new FormData();
+
+        formData.append('file', this.state.file);
+        let response = await fetch('https://b7yutqw6pf.execute-api.us-east-1.amazonaws.com/api/upload', { method: 'POST', body: formData });
+        let response_json = await response.json();
+        this.setState({ urlfile: response_json.file.location })
+    }
     render() {
         return (
-            <Logout>
-                <LogoutButton />
-            </Logout>
+            <div style={{ width: "100%" }}>
+                <img src={this.state.urlfile}></img>
+                <input type="file" name="file" onChange={this.changeHandler} />
+                <button enabled={this.state.submitted} onClick={this.handleSubmission}>Submit</button>
+                <Logout>
+                    <LogoutButton />
+                </Logout>
+            </div>
         )
     }
 }
